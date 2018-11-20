9021e91d65ed41c9d4ea07bac8bf97de2398b5e8
Quentin CHAPEL <chapelquen@gmail.com>
2018-11-20 20:32:16 +0100

    [FEAT]projectInitialisation
    Initialise android studio project

---
+++ app/src/main/java/coderproprement/lpiem/com/projetcoderproprement/MainActivity.java
@@ -0,0 +1,13 @@
+package coderproprement.lpiem.com.projetcoderproprement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
