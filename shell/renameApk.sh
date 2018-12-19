1172d3447704c6fbf29201f6a565855f676eea90
Quentin CHAPEL <chapelquen@gmail.com>
2018-12-19 13:33:43 +0100

    [Feat]Integrating shell function
    Integration shell function "renaming apk" to the fastlane

---
+++ shell/renameApk.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+branch=$(git symbolic-ref --short HEAD)
+newName=$branch.".apk"
+newName=`ècho $newName | sed 's/\//_/g'`
+filePath="../app/build/outputs/apk/debug"
+apkDefaultName="app-debug.apk"
+cd $filePath
+if [ -f $apkDefaultName]
+then
+	mv $apkDefaultName $newAPKName
+else
+	echo "Debug apk not found. Aborting moving process."
+fi
